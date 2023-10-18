5e6b06e50d6a6841eef0a22a3663c80f844c4f82
王东 <www@qs5.org>
2023-10-18 21:57:39 +0800

    首次添加 基本文件

---
+++ build/flarum/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+# 初始化安装 Flarum (首次运行时)
+if [ -z "$(ls -A)" ]; then
+    composer create-project flarum/flarum .
+    mkdir packages
+    composer config repositories.0 path "packages/*"
+fi
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+	set -- php-fpm "$@"
+fi
+
+exec "$@"
